90cc8ac001a24faabcd03a8cda5d14cb6fca7abb
adrozs <adrozsahegyi@gmail.com>
2024-01-10 11:08:06 +0100

    Changelog
    Added models for all tables and the database connections and relations between entities

---
+++ Dreamify/Models/Song.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dreamify.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; } 
+        public string Title { get; set; }
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
