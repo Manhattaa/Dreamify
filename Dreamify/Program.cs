f007ffc0ede16a09ed6cfdf3b54eab7b6c6cd2f6
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-10 11:41:04 +0100

    Patch Notes:
    Added the connection string to Program.cs

--- Dreamify/Program.cs
+++ Dreamify/Program.cs
@@ -1,3 +1,6 @@
+using Dreamify.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Dreamify
 {
     public class Program
@@ -5,6 +8,8 @@ namespace Dreamify
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            string connectionString = builder.Configuration.GetConnectionString("ApplicationContext");
+            builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
