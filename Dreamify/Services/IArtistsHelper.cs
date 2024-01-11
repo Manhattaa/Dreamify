cc4a7acdb5d57a20a0898d5831916af6034acdc8
Fady Hatta <Fadynhatta@gmail.com>
2024-01-11 14:19:24 +0100

    Added IArtistsHelper

---
+++ Dreamify/Services/IArtistsHelper.cs
@@ -0,0 +1,17 @@
+﻿using Dreamify.Data;
+
+namespace Dreamify.Services
+{
+    public interface IArtistsHelper
+    {
+        public IResult AddArtistToDb(ApplicationContext context);
+    }
+
+    public class ArtistsHelper : IArtistsHelper
+    {
+        public IResult AddSongtoDb(ApplicationContext context)
+        {
+            return Results.Ok();
+        }
+    }
+}
