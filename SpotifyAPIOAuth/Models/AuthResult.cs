5dd733f0165db425de3f861ce8224ecaad5693f3
Fady Hatta <Fadynhatta@gmail.com>
2024-01-11 11:30:02 +0100

    Added comments to the AuthResult.

--- SpotifyAPIOAuth/Models/AuthResult.cs
+++ SpotifyAPIOAuth/Models/AuthResult.cs
@@ -1,5 +1,7 @@
 ﻿namespace SpotifyAPIOAuth.Models
 {
+    //The recipient of the result
+    //The Authorization guide implicitly states this in this passage https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
     public class AuthResult
     {
         public string access_token { get; set; }
