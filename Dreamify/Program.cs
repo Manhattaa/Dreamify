5b44a8a4dcba89562249637a20d2319af43294b6
Adrozs <adrozsahegyi@gmail.com>
2024-01-11 15:34:59 +0100

    Added scope for ISongsHelper and endpoint for addsongs

--- Dreamify/Program.cs
+++ Dreamify/Program.cs
@@ -1,4 +1,6 @@
 using Dreamify.Data;
+using Dreamify.Handlers;
+using Dreamify.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dreamify
@@ -10,9 +12,13 @@ namespace Dreamify
             var builder = WebApplication.CreateBuilder(args);
             string connectionString = builder.Configuration.GetConnectionString("ApplicationContext");
             builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+            builder.Services.AddScoped<ISongsHelper, SongsHelper>();
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+
+            app.MapPost("/artists/{artistId}/genre/{genreId}/songs", ArtistHandler.AddSong);
 
             app.Run();
         }
