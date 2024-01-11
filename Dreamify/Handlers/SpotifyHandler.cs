30dca82152d6625fb345871ec96361d3cb78bad7
Fady Hatta <Fadynhatta@gmail.com>
2024-01-11 20:23:09 +0100

    Added some building blocks for the SpotifyAPI

---
+++ Dreamify/Handlers/SpotifyHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Dreamify.Handlers
+{
+    public class SpotifyHandler
+    {
+        public interface ISpotifyHandler
+        {
+            Task<string> GetAccessToken();
+        }
+        public class SpotifyHandler : ISpotifyHandler
+        {
+            private string _clientId;
+            private string _clientSecret;
+            //parameter logic
+            //parameter logic
+            
+            //method for GetAccessToken
+
+            //httpclient logic
+
+           
+        }
+    }
+}
