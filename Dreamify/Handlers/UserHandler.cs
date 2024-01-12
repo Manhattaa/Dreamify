80a18c5eca8eb7d5cc92c8da60723189a8ac9af5
Adrozs <adrozsahegyi@gmail.com>
2024-01-12 22:41:17 +0100

    Added methods for connecting a user to a artist, genre, and song

--- Dreamify/Handlers/UserHandler.cs
+++ Dreamify/Handlers/UserHandler.cs
@@ -1,8 +1,27 @@
-﻿namespace Dreamify.Handlers
+﻿using Dreamify.Models.Dtos;
+using Dreamify.Services;
+
+namespace Dreamify.Handlers
 {
     public class UserHandler
     {
         // Post usernames
+        public static IResult ConnectUserToArtist(int userId, int artistId, IUsersHelper userHelper)
+        {
+            userHelper.ConnectUserToArtist(userId, artistId);
+            return Results.Ok();  // Temp change this later
+        }
 
+        public static IResult ConnectUserToGenre(int userId, int genreId, IUsersHelper userHelper)
+        {
+            userHelper.ConnectUserToGenre(userId, genreId);
+            return Results.Ok();  // Temp change this later
+        }
+
+        public static IResult ConnectUserToSong(int userId, int songId, IUsersHelper userHelper)
+        {
+            userHelper.ConnectUserToSong(userId, songId);
+            return Results.Ok();  // Temp change this later
+        }
     }
 }
