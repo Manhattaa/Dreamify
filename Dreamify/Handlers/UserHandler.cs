75dc6d6613bb898fb489268f59742ee07e28ba34
MalinNyberg <malin.nyberg@chasacademy.se>
2024-01-16 15:27:18 +0100

    The code in UserHandler randomly disapeared so here is the new one.

--- Dreamify/Handlers/UserHandler.cs
+++ Dreamify/Handlers/UserHandler.cs
@@ -2,21 +2,21 @@
 using Dreamify.Models;
 using Dreamify.Models.Dtos;
 using Dreamify.Models.ViewModels;
-
+using Dreamify.Services;
 
 namespace Dreamify.Handlers
 {
     public class UserHandler
     {
-        public static IResult GetUser(ApplicationContext context)
+        public static IResult GetUser(int? userId, IUsersHelper usersHelper)
         {
-            var user = context.Users()
-            .Select(u => new UsersViewModel()
-            {
-                Username = u.UserName,
-            })
-            .ToList();               
+            return usersHelper.GetUser(userId);
         }
 
+        public static IResult AddUser(UsersDto usersDto, IUsersHelper usersHelper)
+        {
+            return usersHelper.AddUser(usersDto);
         }
+
+    }
 }
