701d79d05675991fa98743df6b2a0c57550bc5a0
adrozs <adrozsahegyi@gmail.com>
2024-01-17 14:40:05 +0100

    Commented away method that doesn't seem to be used. Remove if it doesn't break anything

--- Dreamify/Models/ViewModels/ArtistsViewModel.cs
+++ Dreamify/Models/ViewModels/ArtistsViewModel.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public ICollection<SongsViewModel> Title { get; set;}
+        // Commented away since it seems not to be used
+        // public ICollection<SongsViewModel> Title { get; set;}
     }
 }
