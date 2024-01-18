3eef1f0f2e583febc7b0e3fdb7106b666445a75b
Adrozs <adrozsahegyi@gmail.com>
2024-01-18 21:06:40 +0100

    Created viewmodels for spotify search method

---
+++ Dreamify/Models/ViewModels/SpotifyViewModels/SongSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dreamify.Models.ViewModels.SpotifyViewModels
+{
+    public class SongSearchViewModel
+    {
+        public string SpotifySongId { get; set; }
+        public string SongName { get; set; }
+        public ICollection<SongArtistViewModel> Artists { get; set; }
+    }
+}
