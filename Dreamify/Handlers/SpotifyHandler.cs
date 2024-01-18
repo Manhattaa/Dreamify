9739313d2d01162f9e8570b42fb3764302648f54
adrozs <adrozsahegyi@gmail.com>
2024-01-18 10:45:03 +0100

    Removed unecessary class
    Removed SpotifyHandler class and renamed all SpotifyHandlerImp to SpotifyHandler. As there was no reason to have both when only one was used

--- Dreamify/Handlers/SpotifyHandler.cs
+++ Dreamify/Handlers/SpotifyHandler.cs
@@ -9,76 +9,73 @@ using Dreamify.Models.Dtos;
 
 namespace Dreamify.Handlers
 {
-    public class SpotifyHandler
+    public interface ISpotifyHandler
     {
-        public interface ISpotifyHandler
+        Task<string> GetAccessToken();
+    }
+
+    //Change the SpotifyHandler name to SpotifyHandler (Short for implementation) since the enclosing names cannot be the same as the original class
+    public class SpotifyHandler : ISpotifyHandler
+    {
+        private string _clientId;
+        private string _clientSecret;
+        private HttpClient _httpClient;
+        private DateTime _hourlyToken;
+        private string _accessToken;
+
+        //if you only want to provide clientId and clientSecret and use the default HTTPClient i'll use the first constructor
+        public SpotifyHandler(string clientId, string clientSecret) : this(clientId, clientSecret, new HttpClient())
         {
-            Task<string> GetAccessToken();
+
         }
-        //Change the SpotifyHandler name to SpotifyHandlerImp (Short for implementation) since the enclosing names cannot be the same as the original class
-        public class SpotifyHandlerImp : ISpotifyHandler
+
+        //If you want to use your own clientId or clientSecret
+
+        public SpotifyHandler(string clientId, string clientSecret, HttpClient httpClient)
         {
-            private string _clientId;
-            private string _clientSecret;
-            private HttpClient _httpClient;
-            private DateTime _hourlyToken;
-            private string _accessToken;
+            _httpClient = httpClient;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }   
 
-            //if you only want to provide clientId and clientSecret and use the default HTTPClient i'll use the first constructor
-            public SpotifyHandlerImp(string clientId, string clientSecret) : this(clientId, clientSecret, new HttpClient())
+        public async Task<string> GetAccessToken()
+        {
+            var hourlyUpdate = (DateTime.Now - _hourlyToken).TotalMinutes; //https://learn.microsoft.com/en-us/dotnet/api/system.timespan.totalminutes?view=net-8.0
+
+            // https://developer.spotify.com/documentation/web-api/concepts/access-token the access token is only valid for 60 minutes.
+            //As such we need to renew this token on an hourly basis.
+            if (hourlyUpdate > 60 || _accessToken == null)
             {
-
+                return await GetToken(_clientId, _clientSecret);
             }
+            return _accessToken;
+        }
 
-            //If you want to use your own clientId or clientSecret
+        public async Task<string> GetToken(string clientId, string clientSecret)
+        {
+            // new HTTP Request Message as cited in the documentation.
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
 
-            public SpotifyHandlerImp(string clientId, string clientSecret, HttpClient httpClient)
+            // Sets the header of the request to the parameters needed to generate new token
+            request.Headers.Authorization = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")));
+
+            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
             {
-                _httpClient = httpClient;
-                _clientId = clientId;
-                _clientSecret = clientSecret;
-            }
+            {"grant_type", "client_credentials"}
+            });
 
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
 
+            //We need to read the response. it gets returned as a Stream (https://learn.microsoft.com/en-us/dotnet/api/system.io.stream?view=net-8.0)
+            //We then deserialize it to get an object that we can actually use.
+            var responseString = await response.Content.ReadAsStringAsync();
+            var authResult = JsonSerializer.Deserialize<AuthResult>(responseString);
 
-            public async Task<string> GetAccessToken()
-            {
-                var hourlyUpdate = (DateTime.Now - _hourlyToken).TotalMinutes; //https://learn.microsoft.com/en-us/dotnet/api/system.timespan.totalminutes?view=net-8.0
+            return authResult.access_token;
 
-                // https://developer.spotify.com/documentation/web-api/concepts/access-token the access token is only valid for 60 minutes.
-                //As such we need to renew this token on an hourly basis.
-                if (hourlyUpdate > 60 || _accessToken == null)
-                {
-                    return await GetToken(_clientId, _clientSecret);
-                }
-                return _accessToken;
-            }
-
-            public async Task<string> GetToken(string clientId, string clientSecret)
-            {
-                // new HTTP Request Message as cited in the documentation.
-                var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
-
-                // Sets the header of the request to the parameters needed to generate new token
-                request.Headers.Authorization = new AuthenticationHeaderValue(
-                    "Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")));
-
-                request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
-                {
-                {"grant_type", "client_credentials"}
-                });
-
-                var response = await _httpClient.SendAsync(request);
-                response.EnsureSuccessStatusCode();
-
-                //We need to read the response. it gets returned as a Stream (https://learn.microsoft.com/en-us/dotnet/api/system.io.stream?view=net-8.0)
-                //We then deserialize it to get an object that we can actually use.
-                var responseString = await response.Content.ReadAsStringAsync();
-                var authResult = JsonSerializer.Deserialize<AuthResult>(responseString);
-
-                return authResult.access_token;
-
-            }
         }
     }
+    
 }
