574d6893d72735dc8b2962d84325721b6ff7afac
Adrozs <adrozsahegyi@gmail.com>
2024-01-18 21:15:55 +0100

    Created dto's that convert Spotify API JSON input to something we can interract with.

---
+++ Dreamify/Models/Dtos/SpotifyDtos/Songs/SongArtistDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dreamify.Models.Dtos.SpotifyDtos.Songs
+{
+    public class SongArtistDto
+    {
+        [JsonPropertyName("id")]
+        public string ArtistId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string ArtistName { get; set; }
+    }
+}
