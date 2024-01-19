08e90eb28d04f04eebb1913fae9ebcf0f7069d30
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-19 13:09:41 +0100

    Spotify Song related ViewModels.

---
+++ Dreamify/Models/ViewModels/SpotifyViewModels/SongArtistViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Dreamify.Models.Dtos.SpotifyDtos.Songs;
+using System.Text.Json.Serialization;
+
+namespace Dreamify.Models.ViewModels.SpotifyViewModels
+{
+    public class SongArtistViewModel
+    {
+        public string ArtistId { get; set; }
+        public string ArtistName { get; set; }
+
+        public SongArtistViewModel(SongArtistDto artistDto)
+        {
+            ArtistId = artistDto.ArtistId;
+            ArtistName = artistDto.ArtistName;
+        }
+    }
+}
--- Dreamify/Models/ViewModels/UserSongsViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dreamify.Models.ViewModels
-{
-    public class UserSongsViewModel
-    {
-        public string Username { get; set; }
-        public List<SongsViewModel> Songs { get; set; }
-    }
-}
