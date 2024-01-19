1a0516c043ce7d644cc027e315831a698f3ddae1
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-19 12:59:52 +0100

    The logic in the handlers are now more consistent.
    Utilities already exists in Master.

--- Dreamify/Handlers/ArtistHandler.cs
+++ Dreamify/Handlers/ArtistHandler.cs
@@ -1,7 +1,8 @@
-﻿using Dreamify.Data;
-using Dreamify.Models.Dtos;
-using Dreamify.Services;
+﻿using Dreamify.Services;
 using System.Net;
+using Dreamify.Data;
+using Dreamify.Models;
+using Dreamify.Models.Dtos.DreamifyDtos;
 
 namespace Dreamify.Handlers
 {
@@ -11,7 +12,31 @@ namespace Dreamify.Handlers
 
         // Artists
 
-        // Songs
+        public static IResult AddArtist(ApplicationContext context, string personId, ArtistDto artistDto)
+        {
+            try
+            {
+                Artist artist = new Artist
+                {
+                    Name = artistDto.Name,
+                    Description = artistDto.Description
+                };
+                context.Artists.Add(artist);
+                context.SaveChanges();
+                return Results.Ok($"Artist {artist.Name} has been added to the database.");
+            }
+            catch (Exception ex)
+            {
+                return Results.Text($"404: Not found! {ex}");
+            }
+        }
+        public static IResult GetArtist(IArtistsHelper artistHelper)
+        {
+            artistHelper.GetArtists();
+            return Results.StatusCode((int)HttpStatusCode.OK);
+        }
+
+        // Post songs
         public static IResult AddSong(int artistId, int genreId, SongDto song, ISongsHelper songHelper)
         {
             songHelper.AddSong(artistId, genreId, song);
@@ -23,9 +48,17 @@ namespace Dreamify.Handlers
             return Results.Json(songHelper.GetSongs());
         }
 
+        // This is correctly formatted and structured
         public static IResult GetUserSongs(int userId, ISongsHelper songHelper)
         {
-            return Results.Json(songHelper.GetUserSongs(userId));
+            try
+            {
+                return Results.Json(songHelper.GetUserSongs(userId));
+            }
+            catch (Exception ex) 
+            {
+                return Results.Problem(title: "Got exception", detail: ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
