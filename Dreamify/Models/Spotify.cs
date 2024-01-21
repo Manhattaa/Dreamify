8bda0c67e3aa90cd63958e0a60d40d15ce843197
Fady Hatta <Fadynhatta@gmail.com>
2024-01-21 22:16:21 +0100

    Fixed a Typo!

--- Dreamify/Models/Spotify.cs
+++ Dreamify/Models/Spotify.cs
@@ -24,7 +24,7 @@ namespace Dreamify.Models
 
         public class Followers
         {
-            public int total { get; set; }
+            public int Total { get; set; }
         }
 
         public class Item
