448057506927e497560938ff7ecb76ac621989b6
Fady Hatta <Fadynhatta@gmail.com>
2024-01-21 22:09:15 +0100

    Added DeviceViewModel that will handle the info regarding the Device that's currently active. As well as if it's even active.

---
+++ Dreamify/Models/ViewModels/SpotifyViewModels/Player ViewModels/DeviceViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Dreamify.Models.ViewModels.SpotifyViewModels
+{
+    public class DeviceViewModel
+    {
+        public string Id { get; set; }
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int VolumePercent { get; set; }
+    }
+}
