f56ed8a2a43ca55571bee77a2e8188948e6cde48
Fady Hatta <Fadynhatta@gmail.com>
2024-01-21 22:08:26 +0100

    Added CurrentlyPlayingTrackResponseViewModel that will show us information regarding the currently playing Track.

---
+++ Dreamify/Models/ViewModels/SpotifyViewModels/Player ViewModels/ActionsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dreamify.Models.ViewModels.SpotifyViewModels
+{
+    public class ActionsViewModel
+    {
+        public bool Pausing { get; set; }
+        public bool Resuming { get; set; }
+        public bool SkippingNext { get; set; }
+        public bool SkippingPrev { get; set; }
+    }
+}
---
+++ Dreamify/Models/ViewModels/SpotifyViewModels/Player ViewModels/CurrentlyPlayingTrackResponseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Dreamify.Models.ViewModels.DreamifyViewModels;
+using static Dreamify.Models.Spotify;
+
+namespace Dreamify.Models.ViewModels.SpotifyViewModels.Player_ViewModels;
+
+public class CurrentlyPlayingTrackResponseViewModel
+{
+    public Device Device { get; set; }
+    public string RepeatState { get; set; }
+    public bool ShuffleState { get; set; }
+    public int Timestamp { get; set; }
+    public int ProgressMs { get; set; }
+    public bool IsPlaying { get; set; }
+    public ItemViewModel Item { get; set; }
+    public string CurrentlyPlayingType { get; set; }
+    public ActionsViewModel Actions { get; set; }
+    }
+
+
