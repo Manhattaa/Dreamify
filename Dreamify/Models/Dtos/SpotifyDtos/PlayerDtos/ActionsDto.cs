60d501e504395551040476515e4b995a806c9337
Fady Hatta <Fadynhatta@gmail.com>
2024-01-21 22:04:37 +0100

    Added an ActionDto that will handle the commands we send to Spotify.
    Play, Pause, Skip, Replay

---
+++ Dreamify/Models/Dtos/SpotifyDtos/PlayerDtos/ActionsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Dreamify.Models.Dtos.SpotifyDtos.PlayerDtos
+{
+    public class ActionsDto
+    {
+        public string Resuming { get; set; }
+        public string Pausing { get; set; }
+        public string SkippingNext { get; set; }
+        public string SkippingPrev { get; set; }
+    }
+}
