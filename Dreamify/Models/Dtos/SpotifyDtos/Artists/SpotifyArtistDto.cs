7759a96f3132d45f647c35a55c5a419352e2a921
Adrozs <adrozsahegyi@gmail.com>
2024-01-22 14:36:18 +0100

    Remade the SpotifyArtistDto to match the response from Spotify API

--- Dreamify/Models/Dtos/SpotifyDtos/Artists/SpotifyArtistDto.cs
+++ Dreamify/Models/Dtos/SpotifyDtos/Artists/SpotifyArtistDto.cs
@@ -1,13 +1,25 @@
-﻿using System.Text.Json.Serialization;
+﻿using Dreamify.Models.Dtos.SpotifyDtos.Songs;
+using System.Text.Json.Serialization;
 
 namespace Dreamify.Models.Dtos.SpotifyDtos.Artists
 {
     public class SpotifyArtistDto
     {
+        [JsonPropertyName("id")]
+        public string SpotifyArtistId { get; set; }
+
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
-        [JsonPropertyName("id")]
-        public string SpotifyArtistId { get; set; }
+        [JsonPropertyName("followers")]
+        public SpotifyFollowersDto Followers { get; set; }
+
+        [JsonPropertyName("genres")]
+        public List<string> Genres { get; set; }
+
+        [JsonPropertyName("popularity")]
+        public int Popularity { get; set; }
+
+
     }
 }
