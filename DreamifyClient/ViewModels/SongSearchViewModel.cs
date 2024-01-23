4e55e153cdce2f0a4b38a210e6c7f4c2b474482b
Adrozs <adrozsahegyi@gmail.com>
2024-01-23 20:09:04 +0100

    Added viewmodels to be able to deserialize json data from the api

---
+++ DreamifyClient/ViewModels/SongSearchViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DreamifyClient.ViewModels
+{
+    public class SongSearchViewModel
+    {
+        [JsonPropertyName("spotifySongId")]
+        public string SpotifySongId { get; set; }
+
+        [JsonPropertyName("songName")]
+        public string SongName { get; set; }
+
+        private string _allArtists { get; set; }
+
+        [JsonPropertyName("artists")]
+        public List<SongArtistViewModel> AllArtists 
+        { 
+            get { return AllArtists; } 
+            set {
+                List<SongArtistViewModel> _allArtists = value;
+
+                Artist = _allArtists.First();    
+            } 
+        }
+
+        public SongArtistViewModel Artist { get; set; }
+    }
+}
