ec667b43da0b23197fd0c08892327e3207bc2a43
Adrozs <adrozsahegyi@gmail.com>
2024-01-23 15:43:29 +0100

    Created class to contain all spotify calls

---
+++ DreamifyClient/SpotifyFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamifyClient
+{
+    public class SpotifyFunctions
+    {
+        private HttpClient httpClient = new HttpClient();
+        private string apiUrl = "http://localhost:5094"; //change this to some global variable so we don't need to re-declare it in each class
+
+        public async Task<string> SaveSongToDb(string search)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync($"{apiUrl}/search/song/{search}");
+            response.EnsureSuccessStatusCode();
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+        }
+
+
+
+        
+
+
+
+    }
+}
