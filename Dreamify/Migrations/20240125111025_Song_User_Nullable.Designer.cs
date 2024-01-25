2d39dffe6261213c9b89ad5ec77dfc1ba2e1e6e6
Adrozs <adrozsahegyi@gmail.com>
2024-01-25 15:09:35 +0100

    Redid migrations to add nullable properties
    Didn't want to update database so I had to remove migrations and redo them

--- Dreamify/Migrations/20240125111025_Song_User_Nullable.Designer.cs
+++ Dreamify/Migrations/20240125111025_Song_User_Nullable.Designer.cs
@@ -4,6 +4,7 @@ using Dreamify.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dreamify.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240125111025_Song_User_Nullable")]
+    partial class Song_User_Nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
