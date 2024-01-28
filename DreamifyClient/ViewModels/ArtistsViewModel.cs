deedf6b785beeb88e63430fb6489946d711cf214
Adrozs <adrozsahegyi@gmail.com>
2024-01-28 17:46:41 +0100

    Added viewmodel

---
+++ DreamifyClient/ViewModels/ArtistsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DreamifyClient.ViewModels
+{
+    public class ArtistsViewModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+    }
+}
