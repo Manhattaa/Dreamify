26295af9c955ea5c25b03fae1d61283ef67f2263
Adrozs <adrozsahegyi@gmail.com>
2024-01-28 17:47:01 +0100

    Added JsonPropName to viewmodel

--- DreamifyClient/ViewModels/SongsViewModel.cs
+++ DreamifyClient/ViewModels/SongsViewModel.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DreamifyClient.ViewModels
 {
     public class SongsViewModel
     {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
     }
 }
