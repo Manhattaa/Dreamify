d6e256268e6b2cc851bfa17fefa7fc51241d1919
Adrozs <adrozsahegyi@gmail.com>
2024-01-28 17:46:25 +0100

    Added dto for data transfer

---
+++ DreamifyClient/Dtos/GenreDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DreamifyClient.Dtos
+{
+    public class GenreDto
+    {
+        public string Title { get; set; }
+    }
+}
