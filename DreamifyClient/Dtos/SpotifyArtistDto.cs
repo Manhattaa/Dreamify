66148bc7e7850a5f2f7c51e927d05597652681d0
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-29 11:44:58 +0100

    Pushing SpotifyArtistDto to Master.

---
+++ DreamifyClient/Dtos/SpotifyArtistDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DreamifyClient.Dtos
+{
+    public class SpotifyArtistDto
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("id")]
+        public string SpotifyArtistId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string ArtistName { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("genres")]
+        public List<string> Genres { get; set; }
+
+        [JsonPropertyName("popularity")]
+        public int Popularity { get; set; }
+
+        [JsonPropertyName("followers")]
+        public int Followers { get; set; }
+    }
+}
