64ede95ad35c5dac84a7fa5d734521d03430368d
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-29 17:50:09 +0100

    Changed Name to ArtistName in ArtistDtos

--- DreamifyClient/Dtos/ArtistDto.cs
+++ DreamifyClient/Dtos/ArtistDto.cs
@@ -8,7 +8,8 @@ namespace DreamifyClient.Dtos
 {
     public class ArtistDto
     {
-        public string Name { get; set; }
+
+        public string ArtistName { get; set; }
         public string? Description { get; set; }
         public string? SpotifyArtistId { get; set; }
         public int? Popularity { get; set; }
