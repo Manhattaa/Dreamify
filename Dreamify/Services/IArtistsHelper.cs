9183175e60d129bcb4cc4c9c1aa29f51a3ed4c29
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-29 11:42:45 +0100

    Changed the name of the Artist within IArtistsHelper.cs from "Name" to "ArtistName"

--- Dreamify/Services/IArtistsHelper.cs
+++ Dreamify/Services/IArtistsHelper.cs
@@ -28,7 +28,7 @@ namespace Dreamify.Services
         {
             Artist artist = new Artist()
             {
-                Name = artistDto.Name,
+                ArtistName = artistDto.Name,
                 Description = artistDto.Description,
                 Popularity = artistDto.Popularity,
                 Genres = artistDto.Genres
@@ -43,7 +43,7 @@ namespace Dreamify.Services
              List<ArtistsViewModel> artists = _context.Artists
                  .Select(a => new ArtistsViewModel
                  {
-                     Name = a.Name,
+                     ArtistName = a.ArtistName,
                  })
                  .ToList();
              return artists;          
@@ -69,7 +69,7 @@ namespace Dreamify.Services
                 Artists = user.Artists
                 .Select(a => new ArtistsViewModel
                 {
-                    Name = a.Name,
+                    ArtistName = a.ArtistName,
                     Description = a.Description
                 })
                 .ToList()
