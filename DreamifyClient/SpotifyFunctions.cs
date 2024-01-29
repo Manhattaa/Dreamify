f6af0fcd6ace61d56d52dfbd835f9287540b669d
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-29 11:48:16 +0100

    Added the logic to AddArtistViaSearch to SpotifyFunctions

--- DreamifyClient/SpotifyFunctions.cs
+++ DreamifyClient/SpotifyFunctions.cs
@@ -51,7 +51,7 @@ namespace DreamifyClient
 
 
             // List of all artists shown to keep track so no duplicate artists is shown
-            List<string> alreadyShownArtists = new List<string>(); 
+            List<string> alreadyShownArtists = new List<string>();
             foreach (SongSearchViewModel song in result)
             {
                 // Print out info if the list doesn't already contain current artist
@@ -64,10 +64,10 @@ namespace DreamifyClient
                     songList.Add(song);
                 }
             }
-            
+
             // Save selected song number
             int selection = NavFunctions.NavigationSongSearch(songList, artistList, "Press [Enter] on the song you want to save");
-            MenuFunctions.footer(); 
+            MenuFunctions.footer();
 
             // If selection is 1 out of range of the list, then exit was chosen. Exit the method
             if (selection == songList.Count)
@@ -76,7 +76,7 @@ namespace DreamifyClient
                 Thread.Sleep(1000);
                 return;
             }
-                
+
             // Get the selected song
             SongSearchViewModel selectedSong = songList.ElementAt(selection);
 
@@ -108,9 +108,94 @@ namespace DreamifyClient
             response.EnsureSuccessStatusCode();
         }
 
-        internal static Task AddArtistViaSearch(int userId)
+        public static async Task AddArtistViaSearch(int userId)
         {
-            throw new NotImplementedException();
+            // Ensure correct search
+            string search;
+            while (true)
+            {
+                Console.Clear();
+                await Console.Out.WriteAsync("\t\t  Enter search: ");
+                search = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(search))
+                    break;
+
+                Console.WriteLine("\t\t  Invalid search. Try again.");
+            }
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"{_apiUrl}/search/artist/{search}");
+            response.EnsureSuccessStatusCode();
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            List<SpotifyArtistsSearchViewModel>? result = JsonSerializer.Deserialize<List<SpotifyArtistsSearchViewModel>>(jsonResponse);
+
+            await Console.Out.WriteLineAsync($"\t\t  Here are the top results for \"{search}\"");
+            MenuFunctions.divider();
+
+            // List of artists and genres to be displayed for the user ((WIP)
+            List<SpotifyArtistsSearchViewModel> artistList = new List<SpotifyArtistsSearchViewModel>();
+
+            // List of all artists shown to keep track so no duplicate artists are shown
+            List<string> alreadyShownArtists = new List<string>();
+            foreach (SpotifyArtistsSearchViewModel artist in result)
+            {
+                // Print out info if the list doesn't already contain the current artist
+                if (!alreadyShownArtists.Contains(artist.ArtistName))
+                {
+                    alreadyShownArtists.Add(artist.ArtistName);
+                    artistList.Add(artist);
+                }
+            }
+
+            // Save selected artist number
+            int selection = NavFunctions.NavigationArtistSearch(artistList, "Press [Enter] on the artist you want to save");
+            MenuFunctions.footer();
+
+            // If selection is out of the range of the list, then exit was chosen. Exit the method
+            if (selection == artistList.Count)
+            {
+                await Console.Out.WriteLineAsync("\t\t  Exiting artist search");
+                Thread.Sleep(1000);
+                return;
+            }
+
+            // Ensure that artistList is not empty before trying to access elements from it
+            if (artistList.Count > 0)
+            {
+                // Get the selected artist
+                SpotifyArtistsSearchViewModel selectedArtist = artistList.ElementAtOrDefault(selection);
+
+                // Ensure that selectedArtist and its associated properties are not null before using them
+                if (selectedArtist != null && selectedArtist.ArtistName != null)
+                {
+
+                    // Ensure that selectedArtist and its associated properties are not null before using them
+                    if (selectedArtist != null && selectedArtist.ArtistName != null)
+                    {
+                        Console.WriteLine($"Artist: {selectedArtist.ArtistName}");
+                        Console.WriteLine($"Description: ");
+                        Console.WriteLine($"Popularity: {selectedArtist.Popularity}%");
+                        Console.WriteLine($"Followers: {selectedArtist.Followers}");
+                        Console.WriteLine($"Genre: {selectedArtist.Genre.First()}");
+
+                        Console.WriteLine($"\nThe Artist: {selectedArtist} has been added to the Database.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Selected artist or artist details are null. Exiting artist search.");
+                        Thread.Sleep(3000);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No artists to select. Exiting artist search.");
+                    Thread.Sleep(3000);
+                    return;
+                }
+            }
         }
     }
 }
