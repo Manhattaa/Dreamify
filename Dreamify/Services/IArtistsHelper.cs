73ac43b975ce82d83a8d7032c084d66cfaeb2ae9
Adrozs <adrozsahegyi@gmail.com>
2024-01-30 13:56:21 +0100

    Moved genre methods from artist helper to genre helper

--- Dreamify/Services/IArtistsHelper.cs
+++ Dreamify/Services/IArtistsHelper.cs
@@ -9,11 +9,9 @@ namespace Dreamify.Services
     public interface IArtistsHelper
     {
         public void AddArtist(ArtistDto artistDto);
-        public void AddGenre(GenreDto genreDto);
         public List<ArtistsViewModel> GetArtists();
-        public List<GenresViewModel> GetGenres();
         public UserArtistsViewModel GetUserArtists(int userId);
-        public UserGenresViewModel GetUserGenres(int userId);
+
     }
 
     public class ArtistsHelper : IArtistsHelper
@@ -76,56 +74,5 @@ namespace Dreamify.Services
 
             return userArtists;
         }
-
-        public UserGenresViewModel GetUserGenres(int userId)
-        {
-            // Get user and their artists from id
-            User user = _context.Users
-            .Include(u => u.Genres)
-            .Where(u => u.UserId == userId)
-            .Single();
-
-            if (user == null)
-                throw new Exception();
-
-
-            // Create viewmodel consisting of username and a list of all songs
-            UserGenresViewModel userGenres = new UserGenresViewModel
-            {
-                Username = user.Username,
-                Genres = user.Genres
-                .Select(a => new GenresViewModel
-                {
-                    Title = a.Title,
-                })
-                .ToList()  
-            };
-
-            return userGenres;
-        }
-
-        public List<GenresViewModel> GetGenres()
-        {
-            List<GenresViewModel> genres = _context.Genres
-                .Select(g => new GenresViewModel
-                {
-                    Title = g.Title,
-                })
-                .ToList();
-
-            return genres;
-        }
-
-        public void AddGenre(GenreDto genreDto)
-        {
-            Genre genre = new Genre()
-            {
-                Title = genreDto.Title
-            };
-
-            _context.Genres.Add(genre);
-            _context.SaveChanges();
-        }
-
     }
 }
