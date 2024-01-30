37509b12673474619a0f3b91309ebebb2e89316d
Adrozs <adrozsahegyi@gmail.com>
2024-01-30 13:56:00 +0100

    Added exception handling and input param to AddGenre and return type of GetGenres changed handler to genreHandler

--- Dreamify/Handlers/ArtistHandler.cs
+++ Dreamify/Handlers/ArtistHandler.cs
@@ -7,19 +7,31 @@ namespace Dreamify.Handlers
 {
     public class ArtistHandler
     {
-        // Genres
-        public static IResult AddGenre(GenreDto genreDto, IGenresHelper genreHelper)
+        public static IResult AddGenres([FromBody] GenreDto genreDto, IGenresHelper genreHelper)
         {
-            genreHelper.AddGenre(genreDto);
-            return Results.StatusCode((int)HttpStatusCode.Created);
+            try
+            {
+                genreHelper.AddGenre(genreDto);
+                return Results.StatusCode((int)HttpStatusCode.Created);
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(title: "Got exception", detail: ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
+            }
         }
-        public static IResult GetGenre(IGenresHelper genreHelper)
-        {
-            return Results.Json(genreHelper.GetGenre());
-        }
-        // Artists
 
-        public static IResult AddArtist(ApplicationContext context, string personId, ArtistDto artistDto)
+        public static IResult GetGenres(IGenresHelper genreHelper)
+        {
+            try
+            {
+                return Results.Json(genreHelper.GetGenres());
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(title: "Got exception", detail: ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
+            }
+        }
+
         public static IResult AddArtist([FromBody]ArtistDto artistDto, IArtistsHelper artistHelper)
         {
             try
@@ -82,31 +94,6 @@ namespace Dreamify.Handlers
             {
                 return Results.Problem(title: "Got exception", detail: ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
             }
-        }
-
-        public static IResult GetGenres(IArtistsHelper artistHelper)
-        {
-            try
-            {
-                return Results.Json(artistHelper.GetGenres());
-            }
-            catch (Exception ex)
-            {
-                return Results.Problem(title: "Got exception", detail: ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
-            }
-        }
-
-        public static IResult AddGenres([FromBody] GenreDto genreDto, IArtistsHelper artistHelper)
-        {
-            try
-            {
-                artistHelper.AddGenre(genreDto);
-                return Results.StatusCode((int)HttpStatusCode.Created);
-            }
-            catch (Exception ex)
-            {
-                return Results.Problem(title: "Got exception", detail: ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
-            }
-        }
+        }        
     }
 }
