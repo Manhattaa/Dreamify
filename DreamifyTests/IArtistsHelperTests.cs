1f3ffc53a8137cc352f686228b3d04c09c51c50d
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-02-02 13:13:02 +0100

    early phase of Artist Testing

---
+++ DreamifyTests/IArtistsHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using Dreamify.Data;
+using Dreamify.Models;
+using Dreamify.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamifyTests
+{
+    [TestClass]
+    public class IArtistsHelperTests
+    {
+        [TestMethod]
+        public void SpotifyArtistSearch_CheckIfArtistGetsReturned()
+        {
+            //Arrange
+
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("SpotifyArtistSearch_CheckIfArtistGetsReturned")
+                .Options;
+
+            ApplicationContext context = new ApplicationContext(options);
+            IArtistsHelper artistsHelpers = new ArtistsHelper(context);
+
+
+            Artist testingArtist = new Artist()
+            {
+                ArtistName = "ArtistTesting",
+            };
+
+            context.Artists.Add(testingArtist);
+            context.SaveChanges();
+
+            //Act 
+
+
+            //Assert
+
+        }
+    }
+}
