171852d784e1d6008df0301d707414893eb383ea
Adrozs <adrozsahegyi@gmail.com>
2024-02-02 12:26:23 +0100

    Added tests for SongsHelper

--- DreamifyTests/ISongsHelperTests.cs
+++ DreamifyTests/ISongsHelperTests.cs
@@ -1,17 +1,7 @@
 ﻿using Dreamify.Data;
-using Dreamify.Handlers;
 using Dreamify.Models;
-using Dreamify.Models.Dtos;
 using Dreamify.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dreamify;
 using Dreamify.Models.Dtos.DreamifyDtos;
 
 namespace DreamifyTests
@@ -19,87 +9,271 @@ namespace DreamifyTests
     [TestClass]
     public class ISongsHelperTests
     {
+        // GetSongs tests
         [TestMethod]
-        public void GetSongs_ReturnsListOfSongs()
+        public void GetSongs_FindsAllSongsInDb()
         {
-            var mockService = new Mock<ISongsHelper>();
-            ISongsHelper songsHelper = mockService.Object;
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("GetSongs_ReturnsListOfSongsViewModel")
+                .Options;
 
-            songsHelper.GetSongs();
-            
-            
-            // ???
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
+
+
+            Song testSong1 = new Song()
+            {
+                Title = "Test-title1",
+            };
+            Song testSong2 = new Song()
+            {
+                Title = "Test-title2",
+            };
+            Song testSong3 = new Song()
+            {
+                Title = "Test-title3",
+            };
+
+
+            context.Songs.Add(testSong1);
+            context.Songs.Add(testSong2);
+            context.Songs.Add(testSong3);
+            context.SaveChanges();
+
+            // Act
+            var result = songsHelper.GetSongs();
+
+            // Assert
+            Assert.AreEqual(3, result.Count);
         }
 
+        [TestMethod]
+        public void GetSongs_GetsCorrectTitle()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("GetSongs_GetsCorrectTitle")
+                .Options;
+
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
+
+
+            Song testSong = new Song()
+            {
+                Title = "Test-title",
+            };
+
+            context.Songs.Add(testSong);
+            context.SaveChanges();
+
+
+            // Act
+            var result = songsHelper.GetSongs();
+
+            // Assert
+            Assert.AreEqual("Test-title", result.First().Title);
+        }
+
+        [TestMethod]
+        public void GetSongs_ReturnsEmptyListWhenDbIsEmpty()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("GetSongs_ReturnsEmptyListWhenDbIsEmpty")
+                .Options;
+
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
+
+
+            // Act
+            var result = songsHelper.GetSongs();
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        // AddSongs tests
         [TestMethod]
         public void AddSong_AddsNewSongToDb()
         {
             // Arrange
             DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
-                .UseInMemoryDatabase("TestDb")
+                .UseInMemoryDatabase("AddSong_AddsNewSongToDb")
                 .Options;
+
             ApplicationContext context = new ApplicationContext(options);
-            SongsHelper songsHelper = new SongsHelper(context);
+            ISongsHelper songsHelper = new SongsHelper(context);
 
             // Act
-            //songsHelper.AddSong(1, 2, new SongDto
-            //{
-            //    Title = "TestTitle",
-            //});
+            songsHelper.AddSong( new SongDto
+            {
+                Title = "Test-title",
+            });
 
 
             // Assert
             Assert.AreEqual(1, context.Songs.Count());
-
         }
 
         [TestMethod]
         public void AddSong_AddsCorrectInputToDb()
         {
-            // Arrange
+            //    // Arrange
             DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
-                .UseInMemoryDatabase("TestDb")
+                .UseInMemoryDatabase("AddSong_AddsCorrectInputToDb")
                 .Options;
             ApplicationContext context = new ApplicationContext(options);
-            SongsHelper songsHelper = new SongsHelper(context);
+            ISongsHelper songsHelper = new SongsHelper(context);
 
             // Act
-            //songsHelper.AddSong(1, 2, new SongDto
-            //{
-            //    Title = "TestTitle",
-            //});
-
-            Artist testArtist = new() { ArtistId = 1};
-            Genre testGenre = new() { GenreId = 2 };
-
-            Song verification = new()
+            songsHelper.AddSong(new SongDto
             {
-                SongId = 1,
-                Title = "TestTile",
-                Artist = testArtist,
-                Genre =  testGenre
-            };
-
+                Title = "Test-title",
+            });
 
             // Assert
-            Assert.AreEqual(verification, context.Songs.Select(s => new 
-            {
-                s.SongId,
-                s.Title,
-                s.Artist.ArtistId,
-                s.Genre.GenreId
-            })
-            .Single());
-
+            Assert.AreEqual("Test-title", context.Songs.First().Title);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(DbUpdateException))]
+        public void AddSong_ThrowsExceptionWhenTitleIsNull()
+        {
+            //    // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("AddSong_ThrowsExceptionWhenTitleIsNull")
+                .Options;
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
 
-        //public void GetUserSongs(int userId);
+            // Act
+            songsHelper.AddSong(new SongDto
+            {
+                Title = null,
+            });
+        }
+
+        // GetUserSongs tests
+        [TestMethod]
+        public void GetUserSongs_FindsCorrectUserSong()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("GetSongs_GetsCorrectTitle")
+                .Options;
+
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
 
 
+            User testUser = new User()
+            {
+                UserId = 1,
+                Username = "Test-username",
+            };
+
+            Song testSong = new Song()
+            {
+                Title = "Test-title"
+            };
+
+            context.Users.Add(testUser);
+            testUser.Songs.Add(testSong);
+            context.SaveChanges();
 
 
+            // Act
+            var result = songsHelper.GetUserSongs(1);
+
+            // Assert
+            Assert.AreEqual("Test-title", result.Songs.First().Title);
+        }
+
+        [TestMethod]
+        public void GetUserSongs_FindsCorrectAmountOfSongs()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("GetUserSongs_FindsCorrectAmountOfSongs")
+                .Options;
+
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
 
 
+            Song testSong1 = new Song()
+            {
+                Title = "Test-title1"
+            };
+
+            Song testSong2 = new Song()
+            {
+                Title = "Test-title2"
+            };
+
+            Song testSong3 = new Song()
+            {
+                Title = "Test-title3"
+            };
+
+            User testUser = new User()
+            {
+                UserId = 1,
+                Username = "Test-username",
+                Songs = new List<Song>()
+                {
+                    testSong1, testSong2, testSong3
+                }
+            };
+
+            context.Users.Add(testUser);
+            context.SaveChanges();
+
+
+            // Act
+            var result = songsHelper.GetUserSongs(1);
+
+            // Assert
+            Assert.AreEqual(3, result.Songs.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetUserSongs_ThrowsExceptionWHenUserIsNotFound()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("GetUserSongs_FindsCorrectAmountOfSongs")
+                .Options;
+
+            ApplicationContext context = new ApplicationContext(options);
+            ISongsHelper songsHelper = new SongsHelper(context);
+
+
+            Song testSong = new Song()
+            {
+                Title = "Test-title"
+            };
+
+            User testUser = new User()
+            {
+                UserId = 1,
+                Username = "Test-username",
+                Songs = new List<Song>()
+                {
+                    testSong
+                }
+            };
+
+            context.Users.Add(testUser);
+            context.SaveChanges();
+
+
+            // Act
+            var result = songsHelper.GetUserSongs(2);
+        }
     }
 }
